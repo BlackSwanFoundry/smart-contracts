bc2ef0634be243f39142430087a32e6c16b43664
Cypherious Buildtonius <87916704+cypherbuild@users.noreply.github.com>
2021-09-01 22:50:54 -0500

    Add SimpleToken for testing purposes

---
+++ test/1_token_sale_tests.js
@@ -0,0 +1,10 @@
+const TokenSale = artifacts.require("bsc/TokenSale");
+const SimpleToken = artifacts.require("bsc/SimpleToken");
+contract("TokenSale", accounts => {
+    it("Should ")
+    it("Schedule token sale.", () => {
+        TokenSale
+        .deployed()
+        .then(instance => instance.create())
+    });
+});
