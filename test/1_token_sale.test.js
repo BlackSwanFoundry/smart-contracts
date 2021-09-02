a8d9d00af9bb12fb49148effea486cfedc3bff50
Dalton L Brady <cypher.build@protonmail.com>
2021-09-02 13:15:57 -0500

    Refactor Tests
    Add in simple token test.
    Refactor token sale tests.

---
+++ test/1_token_sale.test.js
@@ -0,0 +1,34 @@
+// Helpers
+const {expect} = require("chai");
+const { BN, expectEvent, expectRevert, constants } = require('@openzeppelin/test-helpers');
+
+// Contracts
+const SimpleToken = artifacts.require("bsc/SimpleToken");
+const TokenSale = artifacts.require("bsc/TokenSale");
+
+contract("SimpleToken", function([creator, other]){
+    const _name = "SimpleToken";
+    const _symb = "SIMP"
+    const _supp = new BN(100);
+
+    beforeEach(async function(){
+        this.token = await SimpleToken.new(_name, _sym, _supp, { from: creator });
+    });
+
+    it("Has total supply", async function(){
+        // Use large integer comparisons
+        expect(await this.token.totalSupply()).to.be.bignumber.equal(_supp);
+    });
+
+    it("Has a name", async function(){
+        expect(await this.token.name()).to.be.equal(_name);
+    });
+
+    it('Has a symbol', async function () {
+        expect(await this.token.symbol()).to.be.equal(_symb);
+    });
+
+    it('Assigns the initial total supply to the creator', async function () {
+        expect(await this.token.balanceOf(creator)).to.be.bignumber.equal(TOTAL_SUPPLY);
+    });
+});
