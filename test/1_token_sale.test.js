b61024dbcd28cef2fc25651588300c7313959d25
Cypherious Buildtonius <87916704+cypherbuild@users.noreply.github.com>
2021-09-09 23:09:05 -0500

    Update 1_token_sale.test.js
    More testing change, still need to workout ether() conversions / rate calculations. I may just end up increasing the token supply values just to play nice for now. Or maybe I figure out how to scale it / make it configurable.

--- test/1_token_sale.test.js
+++ test/1_token_sale.test.js
@@ -19,18 +19,29 @@ contract('TokenSale', function ([ creator, investor ]) {
     beforeEach(async function(){
       this.token = await Token.new(_name, _symbol, _supply, _deci, {from: creator});
       this.tokenSale = await TokenSale.new(_rate, creator, this.token.address, {from: creator});
+
+      this.token.transfer(this.tokenSale.address, new BN(500000));
     });
   
     it('should create crowdsale with correct parameters', async function(){
-        
         expect(await this.tokenSale.rate()).to.be.bignumber.equal(_rate);
         expect(await this.tokenSale.wallet()).to.be.equal(creator);
         expect(await this.tokenSale.token()).to.be.equal(this.token.address);
+        expect(await this.token.balanceOf(this.tokenSale.address)).to.be.bignumber.equal(new BN(500000));
     });
   
     it('should accept payments', async function () {
-      const investmentAmount = ether('1');
-      const expectedTokenAmount = _rate.mul(investmentAmount);
+      const investmentAmount = ether('.01');
+      console.log(investmentAmount.toString());
+      const expectedTokenAmount = investmentAmount.mul(_rate).div(new BN(10**15));
+
+      console.log(_rate.toString());
+      console.log(expectedTokenAmount.toString());
+
+      let creatorBalance = await this.token.balanceOf(creator);
+      console.log(creatorBalance.toString());
+      let contractBalance = await this.token.balanceOf(this.tokenSale.address);
+      console.log(contractBalance.toString());
   
       await this.tokenSale.buyTokens(investor, { value: investmentAmount, from: investor });
   
